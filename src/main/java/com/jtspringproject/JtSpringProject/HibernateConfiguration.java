2ac213b2760d7fb206324bbff7ed48a338a66476
Kavya <mkavya1231@gmail.com>
2026-08-21 17:02:01 +0530

    Refactor Hibernate configuration for JPA support

--- src/main/java/com/jtspringproject/JtSpringProject/HibernateConfiguration.java
+++ src/main/java/com/jtspringproject/JtSpringProject/HibernateConfiguration.java
@@ -1,72 +1,154 @@
 package com.jtspringproject.JtSpringProject;
- 
+
 import java.util.Properties;
- 
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
- 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
- 
+
 @Configuration
 @EnableTransactionManagement
 public class HibernateConfiguration {
+
     @Value("${db.driver}")
     private String DRIVER;
- 
+
     @Value("${db.password}")
     private String PASSWORD;
- 
+
     @Value("${db.url}")
     private String URL;
- 
+
     @Value("${db.username}")
     private String USERNAME;
- 
+
     @Value("${hibernate.dialect}")
     private String DIALECT;
- 
+
     @Value("${hibernate.show_sql}")
     private String SHOW_SQL;
- 
+
     @Value("${hibernate.hbm2ddl.auto}")
     private String HBM2DDL_AUTO;
- 
+
     @Value("${entitymanager.packagesToScan}")
     private String PACKAGES_TO_SCAN;
- 
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
         dataSource.setDriverClassName(DRIVER);
         dataSource.setUrl(URL);
         dataSource.setUsername(USERNAME);
         dataSource.setPassword(PASSWORD);
+
         return dataSource;
     }
- 
+
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        LocalSessionFactoryBean sessionFactory =
+                new LocalSessionFactoryBean();
+
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan(PACKAGES_TO_SCAN);
+
         Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", DIALECT);
-        hibernateProperties.put("hibernate.show_sql", SHOW_SQL);
-        hibernateProperties.put("hibernate.hbm2ddl.auto", HBM2DDL_AUTO);
-        sessionFactory.setHibernateProperties(hibernateProperties);
- 
+
+        hibernateProperties.put(
+                "hibernate.dialect",
+                DIALECT
+        );
+
+        hibernateProperties.put(
+                "hibernate.show_sql",
+                SHOW_SQL
+        );
+
+        hibernateProperties.put(
+                "hibernate.hbm2ddl.auto",
+                HBM2DDL_AUTO
+        );
+
+        sessionFactory.setHibernateProperties(
+                hibernateProperties
+        );
+
         return sessionFactory;
     }
- 
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        LocalContainerEntityManagerFactoryBean factory =
+                new LocalContainerEntityManagerFactoryBean();
+
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan(PACKAGES_TO_SCAN);
+        factory.setPersistenceUnitName("default");
+
+        HibernateJpaVendorAdapter vendorAdapter =
+                new HibernateJpaVendorAdapter();
+
+        vendorAdapter.setShowSql(
+                Boolean.parseBoolean(SHOW_SQL)
+        );
+
+        factory.setJpaVendorAdapter(vendorAdapter);
+
+        Properties properties = new Properties();
+
+        properties.put(
+                "hibernate.dialect",
+                DIALECT
+        );
+
+        properties.put(
+                "hibernate.hbm2ddl.auto",
+                HBM2DDL_AUTO
+        );
+
+        properties.put(
+                "hibernate.show_sql",
+                SHOW_SQL
+        );
+
+        factory.setJpaProperties(properties);
+
+        return factory;
+    }
+
+    @Bean
+    public JpaTransactionManager jpaTransactionManager(
+            EntityManagerFactory entityManagerFactory) {
+
+        return new JpaTransactionManager(
+                entityManagerFactory
+        );
+    }
+
     @Bean
     public HibernateTransactionManager transactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
+
+        HibernateTransactionManager transactionManager =
+                new HibernateTransactionManager();
+
+        transactionManager.setSessionFactory(
+                sessionFactory().getObject()
+        );
+
         return transactionManager;
-    }   
+    }
 }
